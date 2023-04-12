0ef2bc70393880b77b0978f18c9e14bcfbdb84ff
Oleksandr Mamchur <irex1988@gmail.com>
2023-04-13 00:36:30 +0300

    add index view

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Movie, Actor, Genre
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_movies = Movie.objects.count()
+    num_actors = Actor.objects.count()
+    num_genres = Genre.objects.count()
+    context = {
+        "num_movies": num_movies,
+        "num_actors": num_actors,
+        "num_genres": num_genres
+    }
+    return render(request, "catalog/index.html", context=context)
+
