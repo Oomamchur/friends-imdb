8c5ed79ce2e95252fc8190c14204e894ff6cab03
Oleksandr Mamchur <irex1988@gmail.com>
2023-04-14 15:56:09 +0300

    add authentification add login/logout templates

--- friends_imdb/urls.py
+++ friends_imdb/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
     path('__debug__/', include('debug_toolbar.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
