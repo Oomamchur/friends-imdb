ddd750fbad4d8810dc45d39b9b1aa16289b566c8
Oleksandr Mamchur <irex1988@gmail.com>
2023-04-20 22:06:34 +0300

    add test_genre_views.py with tests for GenreViews

---
+++ tests/test_genre_views.py
@@ -0,0 +1,71 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from catalog.models import Genre
+
+GENRE_URL = reverse("catalog:genre-list")
+
+
+class PublicGenreTests(TestCase):
+    def setUp(self) -> None:
+        self.genre = Genre.objects.create(name="test")
+
+    def test_genre_list(self) -> None:
+        response = self.client.get(GENRE_URL)
+        genres = Genre.objects.all()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(list(response.context["genre_list"]), list(genres))
+        self.assertTemplateUsed(response, "catalog/genre_list.html")
+
+    def test_genre_detail(self) -> None:
+        response = self.client.get(reverse(
+            "catalog:genre-detail",
+            kwargs={"pk": self.genre.pk}
+        ))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "catalog/genre_detail.html")
+        self.assertContains(response, self.genre.id)
+
+    def test_no_acces_to_update_without_login(self) -> None:
+        response = self.client.get(reverse(
+            "catalog:genre-update",
+            kwargs={"pk": self.genre.pk}
+        ))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response,
+            "/accounts/login/?next=/genres/1/update"
+        )
+
+
+class PrivateGenreTests(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="test",
+            password="test1234"
+        )
+        self.client.force_login(self.user)
+        self.genre = Genre.objects.create(name="test")
+
+    def test_access_to_update_with_login(self) -> None:
+        response = self.client.get(reverse(
+            "catalog:genre-update",
+            kwargs={"pk": self.genre.pk}
+        ))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "catalog/genre_form.html")
+
+    def test_update_genre(self) -> None:
+        response = self.client.post(
+            reverse("catalog:genre-update", kwargs={"pk": self.genre.pk}),
+            {"name": "Comedy"}
+        )
+        updated_genre = Genre.objects.get(pk=self.genre.pk)
+
+        self.assertRedirects(response, reverse("catalog:genre-list"))
+        self.assertEqual(updated_genre.name, "Comedy")
