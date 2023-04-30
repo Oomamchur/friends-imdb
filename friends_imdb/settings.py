431f0e3442cf29ebe50c3a06a1cc669a82d0aadb
Oleksandr Mamchur <irex1988@gmail.com>
2023-04-30 21:22:27 +0300

    Add friends-imdb.onrender.com to ALLOWED_HOSTS

--- friends_imdb/settings.py
+++ friends_imdb/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "friends-imdb.onrender.com"]
 
 INTERNAL_IPS = [
     # ...
